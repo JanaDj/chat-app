259b4fa48ff0d1d3995b1a006c5f00e18e676e55
Jana Djordjevic <jana.dj.12@hotmail.com>
2020-02-28 23:19:38 +0100

    updated app styles, added conf file example for nginx, added feature to load message history, added scroll option

--- client.js
+++ client.js
@@ -3,36 +3,65 @@ const messageContainer = document.getElementById('chatContainer')
 const messageForm = document.getElementById('sendMsgContainer')
 const messageInput = document.getElementById('messageInput')
 
+// load previous messages:
+const getUrl = 'http://127.0.0.1:3000/messages';
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function () {
+  if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+    console.log(xmlHttp.responseText);
+        let msgs = xmlHttp.responseText;
+        msgs = msgs.replace('[', '');
+        msgs = msgs.replace(/\"/g, '');
+        msgs = msgs.replace(']', '');
+        let values = msgs.split(',');
+        values.forEach(msg => {
+          if(msg){    // to avoid displaying empty message bubbles
+      appendMessage(msg, 'historyMsg');
+          }
+    });
+  }
+}
+xmlHttp.open("GET", getUrl, true); // true for asynchronous 
+xmlHttp.send(null);
+
+
+
+
+
+
+
 const href = window.location.href;
 const url = new URL(href);
 const name = url.searchParams.get('nameInput');
-console.log(name);
-appendMessage('You joined', 'chatInfo')
-socket.emit('new-user', name)
+appendMessage('You joined', 'chatInfo');
+socket.emit('new-user', name);
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`, 'otherMsg');
 })
 
 socket.on('user-connected', name => {
-  appendMessage(`${name} connected`, 'chatInfo')
+  appendMessage(`${name} connected`, 'chatInfo');
 })
 
 socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`, 'chatInfo')
+  appendMessage(`${name} disconnected`, 'chatInfo');
 })
 
 messageForm.addEventListener('submit', e => {
   e.preventDefault();
-  const message = messageInput.value
-  appendMessage(`You: ${message}`, 'ownMsg')
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`, 'ownMsg');
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
 })
 
 function appendMessage(message, className) {
   const messageElement = document.createElement('div');
   messageElement.classList.add(className);
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+
+  //scroll div to the bottom:
+  messageContainer.scrollTop = messageContainer.scrollHeight;
 }
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+// const router = express.Router();
 const io = require('socket.io')(http);
 const port = 3000;
 const redis = require('redis');
-const redisClient = redis.createClient({host: 'localhost', port: 6379})
+const redisClient = redis.createClient({ host: 'localhost', port: 6379 });
 const redisAdapter = require('socket.io-redis');
-io.adapter(redisAdapter({host: 'localhost', port: 6379})); 
+io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
 const os = require('os');
 const cluster = require('cluster');
 const clusterWorkSize = os.cpus().length;
 
 // setting up the multiple cpus 
-if (clusterWorkSize > 1){
-  if(cluster.isMaster) {
+if (clusterWorkSize > 1) {
+  if (cluster.isMaster) {
     for (let i = 0; i < clusterWorkSize; i++) {
       cluster.fork();
     }
@@ -23,13 +24,54 @@ if (clusterWorkSize > 1){
   } else {
     http.listen(port, () => {
       console.log(`Express server listening on port ${port} and worker ${process.pid}`);
+      //  clears redis msgs on server start:  (used while testing)
+      redisClient.flushall();
     });
   }
 } else {
-    http.listen(port, () => {
-      console.log(`Express server listening on port ${port} with the single worker ${process.pid}`);
-    });
+  http.listen(port, () => {
+    console.log(`Express server listening on port ${port} with the single worker ${process.pid}`);
+    redisClient.flushall();
+  });
 }
+
+//  handling CORS 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+/**
+ * Method to wrap the try catch code to reduce repetative code
+ * @param {function} cb 
+ */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+//  returns all messages stored in redis
+app.get('/messages', asyncHandler(async (req, res) => {
+  //get msgs
+  let msgs = redisClient.lrange('messages', 0, 99, (err, messageList) => {
+    if (!err) {
+      let result = [];
+      messageList = messageList.reverse();
+      for (let msg in messageList) {
+        parsedMsg = JSON.parse(messageList[msg]);
+        msgText = `${parsedMsg['name']} : ${parsedMsg['message']}`;
+        result.push(msgText);
+        // console.log(msgText);
+      }
+      res.send(result).end();
+    } 
+  })
+
+}));
 // storing connected users
 const users = {};
 
@@ -39,26 +81,31 @@ redisClient.on('ready', () => {
 });
 
 redisClient.on('error', () => {
-  console.log("Error in Redis"); 
+  console.log("Error in Redis");
 });
 // socket events handled here 
 io.on('connection', socket => {
   console.log('user is connected');
-    // new user joined
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
-      })
-    //   new message 
-      socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-      })
-    //   user left the chat
-      socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
-      })
+  // new user joined
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  })
+  //   new message 
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+    // `${data.name}: ${data.message}`
+    console.log(JSON.stringify({ message: message, name: users[socket.id] }));
+    redisClient.lpush('messages', JSON.stringify({ message: message, name: users[socket.id] }));
+    redisClient.ltrim('messages', 0, 99);  //storing only last 100 messages
+  })
+  //   user left the chat
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  })
 });
+
 
 
 
